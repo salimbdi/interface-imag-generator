fc4a5099e676932d20526bf6c5b4be3625d9c8f8
Star_Bs_dev <s_bdirina@estin.dz>
2025-02-07 00:15:29 +0100

    Add files via upload

---
+++ consept.js
@@ -0,0 +1,9 @@
+const api = "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";
+const inp = document.getElementById('inp');
+const img = document.getElementById('.images');
+
+
+function getimage() 
+{
+    console.log("salim")
+}
